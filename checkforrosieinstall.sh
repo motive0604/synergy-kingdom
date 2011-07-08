668fdce5000240a861a33484992fc4b26c7d432c
trevor.eckhart@gmail.com <trevor.eckhart@gmail.com@e27db7fd-e457-09b0-7121-62db74fd60a2>
2011-07-08 00:56:43 +0000

    script cleanup so init.d works cleanly again upgrade cleanup script now kicksass (i upgraded like a year old nandroid with no FC) network DNS tweaks for faster page load GPS tweaks for faster GPS sync
    battery almost dead hope this makes it

--- checkforrosieinstall.sh
+++ checkforrosieinstall.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 if [ -e /sdcard/SynergyROM/rosie ]; then
-	busybox unzip /cache/rosie.zip -d /;
+	/sbin/busybox unzip /cache/rosie.zip -d /;
 	rm /cache/rosie.zip;
 else
 	rm /cache/rosie.zip;
