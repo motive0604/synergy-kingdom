c5863bfd074f590d27a57777530c7b1231a77dd4
trevor.eckhart@gmail.com <trevor.eckhart@gmail.com@e27db7fd-e457-09b0-7121-62db74fd60a2>
2011-07-09 04:53:23 +0000

    fix up dropbear for real this time (i hope)   :)

--- trunk/data/firstboot.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh.old
-#
-# modded by treve
-SYNERGYROM=1;
-mkdir -p /data/dropbear;
-chmod 700 /data/dropbear;
-dropbearkey -t dss -f /data/dropbear/dropbear_dss_host_key;
-dropbearkey -t rsa -f /data/dropbear/dropbear_rsa_host_key;
